654001b2d078b9344d891ef1524d73bd35d18e78
Roly-Yuva <codedyuva333@gmail.com>
2021-11-08 22:54:21 -0500

    Created multiple files

---
+++ src/include/enemy_database.lua
@@ -0,0 +1,13 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+M.lv1 = {
+	-- level 1 enemies here
+}
+
+-- continue for all levels
+
+return M
