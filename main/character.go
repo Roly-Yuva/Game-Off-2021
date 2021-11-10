1f35a67a3742f9a5da05a30ce3eb7857ecaec366
Roly-Yuva <codedyuva333@gmail.com>
2021-11-09 22:07:53 -0500

    Created multiple files

---
+++ main/character.go
@@ -0,0 +1,45 @@
+components {
+  id: "character_passive"
+  component: "/src/scripts/character_passive.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "character_equip"
+  component: "/src/scripts/character_equip.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "character_battle"
+  component: "/src/scripts/character_battle.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
