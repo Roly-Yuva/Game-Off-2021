b013eed433601e3e0b598ea35632b68a30a4b1d8
Roly-Yuva <codedyuva333@gmail.com>
2021-11-17 19:18:03 -0500

    Created multiple files

---
+++ src/include/equip.lua
@@ -0,0 +1,11 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+function M.equip_head(equipped, new_head)
+	
+end
+
+return M
